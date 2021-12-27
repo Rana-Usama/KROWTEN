01fc83fa28607c915959bc8e798d0bb9b7c38d32
RANA-USAMA <ranausamauet3430@gmail.com>
2021-12-28 02:42:32 +0500

    notification screen

--- App.js
+++ App.js
@@ -5,15 +5,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 //screens
 import RegisterScreen from './app/screens/RegisterScreen';
 import ConnectScreen from './app/screens/ConnectScreen';
+import NotificationsScreen from './app/screens/NotificationsScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator headerMode="none" initialRouteName="ConnectScreen">
+      <Stack.Navigator headerMode="none" initialRouteName="NotificationsScreen">
         <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         <Stack.Screen name="ConnectScreen" component={ConnectScreen} />
+        <Stack.Screen name="NotificationsScreen" component={NotificationsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
